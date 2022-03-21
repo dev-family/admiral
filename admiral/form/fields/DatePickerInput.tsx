cbbe49fee3b0afd0afa14adceea142387320fcfc
a.kashina <a.kashina@websecret.by>
2022-03-21 14:50:21 +0300

    feat: DatePicker Input

---
+++ admiral/form/fields/DatePickerInput.tsx
@@ -0,0 +1,34 @@
+import React, { useCallback } from 'react'
+import { useForm } from '../FormContext'
+import { Form } from '../Form'
+import { DatePicker } from '@/admiral/ui'
+import { PickerProps } from '@/admiral/ui/DatePicker/generatePicker/interfaces'
+import { FormItemProps } from '../Item'
+import parseISO from 'date-fns/parseISO'
+
+type DatePickerInputProps = FormItemProps & {
+    name: string
+} & PickerProps<Date>
+
+export const DatePickerInput: React.FC<DatePickerInputProps> = ({
+    name,
+    label,
+    required,
+    columnSpan,
+    ...pickerProps
+}) => {
+    const { values, errors, setValues } = useForm()
+
+    let value = values[name] ? parseISO(values[name]) : null
+    const error = errors[name]?.[0]
+
+    const onChange = useCallback((value: Date | null) => {
+        setValues((values: any) => ({ ...values, [name]: value?.toISOString() }))
+    }, [])
+
+    return (
+        <Form.Item label={label} required={required} error={error} columnSpan={columnSpan}>
+            <DatePicker {...pickerProps} value={value} onChange={onChange} alert={!!error} />
+        </Form.Item>
+    )
+}
--- admiral/form/index.tsx
+++ admiral/form/index.tsx
@@ -1,5 +1,6 @@
 export * from './Form'
 export * from './fields/BooleanInput'
+export * from './fields/DatePickerInput'
 export * from './fields/MultilineTextInput'
 export * from './fields/TextInput'
 export * from './fields/SelectInput'

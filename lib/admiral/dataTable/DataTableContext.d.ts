e3e87b3f4dabeb6624eaa7c0797b3c1e8e7f683c
a.kashina <a.kashina@websecret.by>
2022-03-30 18:20:27 +0300

    build: lib (v0.2.1)

---
+++ lib/admiral/dataTable/DataTableContext.d.ts
@@ -0,0 +1,10 @@
+import React from 'react'
+declare type DataTableValueType = {
+    refresh: () => void
+}
+export declare const DataTableContext: React.Context<DataTableValueType>
+export declare const DataTableContextProvider: React.FC<{
+    value: DataTableValueType
+}>
+export declare function useDataTable(): DataTableValueType
+export {}

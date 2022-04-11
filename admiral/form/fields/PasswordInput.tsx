cd121a501b98bfb5b56065a6e68d2f1ca22b9901
a.kashina <a.kashina@websecret.by>
2022-04-11 15:51:29 +0300

    feat: (admin, form, fields) PasswordInput

---
+++ admiral/form/fields/PasswordInput.tsx
@@ -0,0 +1,38 @@
+import React, { useCallback } from 'react'
+import { useForm } from '../FormContext'
+import { Form } from '../Form'
+import { Password } from '../../ui'
+import type { InputProps } from '../../ui/Input/interfaces'
+import { FormItemProps } from '../Item'
+
+export interface PasswordInputProps extends InputProps, FormItemProps {
+    name: string
+}
+
+export const PasswordInput: React.FC<PasswordInputProps> = ({
+    name,
+    label,
+    required,
+    columnSpan,
+    ...inputProps
+}) => {
+    const { values, errors, setValues } = useForm()
+    const value = values[name]
+    const error = errors[name]?.[0]
+
+    const onChange = useCallback((e) => {
+        setValues((values: any) => ({ ...values, [name]: e.target.value }))
+    }, [])
+
+    return (
+        <Form.Item label={label} required={required} error={error} columnSpan={columnSpan}>
+            <Password
+                {...inputProps}
+                name={name}
+                value={value}
+                onChange={onChange}
+                alert={!!error}
+            />
+        </Form.Item>
+    )
+}

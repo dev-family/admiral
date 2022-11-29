e0dc9e7d6af0cbab20892d12c0236a18ce741935
Yana Korpan <y.korpan@websecret.by>
2022-11-29 12:39:52 +0400

    ADM-72:(feat) add user info hook

--- admiral/auth/index.ts
+++ admiral/auth/index.ts
@@ -1,5 +1,6 @@
 export { default as useAuthState } from './useAuthState'
 export { default as useCheckAuth } from './useCheckAuth'
 export { default as useGetIdentity } from './useGetIdentity'
+export { default as useIdentityState } from './useIdentityState'
 export { default as useLogin } from './useLogin'
 export { default as useLogout } from './useLogout'

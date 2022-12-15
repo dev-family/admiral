f1e9786c6afd422c40ee2b71e3e0f2af83a4c6b0
Yana Korpan <y.korpan@websecret.by>
2022-12-15 19:38:22 +0400

    ADM-78: (fix) CRUDlocale

--- src/crud/users/users-ru.tsx
+++ src/crud/users/users-ru.tsx
@@ -26,29 +26,6 @@ export const resource = 'users'
 export const UsersCRUD = createCRUD({
     path,
     resource,
-    locale: {
-        actions: {
-            submit: 'Сохранить',
-            back: 'Назад',
-            tableColumn: 'Действия',
-            paginationTotal: (total) => `Всего ${total}`,
-        },
-        pagination: {
-            items_per_page: '/ стр.',
-            jump_to: 'Перейти',
-            jump_to_confirm: 'подтвердить',
-            page: 'Страница',
-            prev_page: 'Назад',
-            next_page: 'Вперед',
-            prev_5: 'Предыдущие 5',
-            next_5: 'Следующие 5',
-            prev_3: 'Предыдущие 3',
-            next_3: 'Следующие 3',
-        },
-        form: {
-            fields: { array: { add: 'Добавить', remove: 'Удалить' } },
-        },
-    },
     index: {
         title: 'Пользователи',
         newButtonText: 'Создать',

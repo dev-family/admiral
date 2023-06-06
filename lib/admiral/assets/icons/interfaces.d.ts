1002eb08c34a686a41d2615d0a6f3061b8307a0f
Arthur Volokhin <arthurvolokhin@MacBook-Air-Arthur.local>
2023-06-06 17:40:04 +0300

    (Build) Lib for v5.3.3 release

--- lib/admiral/assets/icons/interfaces.d.ts
+++ lib/admiral/assets/icons/interfaces.d.ts
@@ -8,5 +8,5 @@ export interface IconProps {
 export interface IconWrapperProps extends IconProps {
     name: IconName;
 }
-export declare type IconName = 'dev-family-logo' | 'dev-family-logo-inversion' | 'dev-family-logo-inversion-bw' | 'spinner';
+export declare type IconName = 'dev-family-logo' | 'dev-family-logo-inversion' | 'dev-family-logo-inversion-bw' | 'logo-auth' | 'logo-auth-inversion' | 'spinner';
 export declare type IconComponentType = ((props: IconProps) => JSX.Element) | null;
--- lib/admiral/ui/DatePicker/locale/ru_RU.d.ts
+++
@@ -1,3 +0,0 @@
-import { PickerLocale } from '../generatePicker/interfaces';
-declare const locale: PickerLocale;
-export default locale;

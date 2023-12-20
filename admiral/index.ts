6e51a387ff0f588e8781fe8a4b526a8adeeb2fa5
Андрей Мурашко <a.murashko@websecret.by>
2023-12-20 13:13:40 +0300

    ADM-62: (Feat) export admiralLocales from entry point

--- admiral/index.ts
+++ admiral/index.ts
@@ -73,3 +73,6 @@ export { createRoutesFrom } from './router'
 
 export { useTheme, useThemeVars } from './theme'
 export type { ThemeName, ThemePreset } from './theme/interfaces'
+
+export { admiralLocales } from './locales'
+export type { AdmiralLocales } from './locales'

6bffe368b491150eeef6ab296de7507c8781f179
Arthur Volokhin <arthurvolokhin@MacBook-Air-Arthur.local>
2024-03-25 12:21:55 +0300

    (Build) Lib for v5.9.5

--- lib/admiral/utils/hooks/index.d.ts
+++ lib/admiral/utils/hooks/index.d.ts
@@ -3,3 +3,4 @@ export { default as useMedia } from './useMedia';
 export { default as useMergedState } from './useMergedState';
 export { default as useSafeSetState } from './useSafeSetState';
 export { default as useUrlState } from './useUrlState';
+export { default as useUpdateEffect } from './useUpdateEffect';
---
+++ lib/admiral/utils/hooks/useUpdateEffect.d.ts
@@ -0,0 +1,3 @@
+import type { DependencyList, EffectCallback } from 'react';
+declare const useUpdateEffect: (callback: EffectCallback, dependencies: DependencyList) => void;
+export default useUpdateEffect;
